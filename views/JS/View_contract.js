580349240c70884eaac96439fe9761eeff936c04
Sandaruwan Gamage <sandaruwangamageggss@gmail.com>
2021-10-29 13:28:50 +0530

    Updated sessions and make css parts

--- views/JS/View_contract.js
+++ views/JS/View_contract.js
@@ -2,3 +2,8 @@ function contractProviderProfile(){
     
     window.open("http://localhost/ict_jobseeker_44/Contracts/Contract_provider_profile","_self");
 }
+
+function BidContract(){
+    
+    window.open("http://localhost/ict_jobseeker_44/Jobseeker/BidContract","_self");
+}
