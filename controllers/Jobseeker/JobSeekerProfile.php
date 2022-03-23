3123308322e0d87e9e650e80b3dcbee94a080ee1
Sandaruwan Gamage <sandaruwangamageggss@gmail.com>
2022-03-24 05:00:14 +0530

    Updated Jobseeker accounnt and profile

--- controllers/Jobseeker/JobSeekerProfile.php
+++ controllers/Jobseeker/JobSeekerProfile.php
@@ -13,7 +13,41 @@ class JobSeekerProfile extends Controller
         #echo "Hello from the Test controller - Index Method";
 
         //pass view name
-        $this->view ->render('JobSeekerProfile'); 
+        // $this->view ->render('JobSeekerProfile'); 
+     
+          $data = $this->model->get_jobseeker_data_m($_GET['User']);
+    //    print_r($data);
+          //pass view name
+          $this->view->render2('JobSeekerProfile',$data);
+      }
+    //   else
+    //   {
+    //        $this->view->render("YouDontHavePermission");
+    //       echo "you dont have permission";
+    //   }
+    function get_JobSeeker_data()
+    {
+
+//        if($_SESSION['User_type'] == 'Counsellor'){
+//            $User_ID = $_SESSION['User_ID'];
+//            $data = $this->model->get_counsellor_data_m($User_ID);
+//            echo json_encode($data);
+//        }
+
+
+    }
+
+
+    function get_JobSeeker_data_admin($User_ID)
+    {
+        $data = $this->model->get_jobseeker_data_m($User_ID);
+        echo json_encode($data);
+        $this->view->render2('JobSeekerProfile',$data);
+
+    }
         
     }
-}
+
+
+
+  
