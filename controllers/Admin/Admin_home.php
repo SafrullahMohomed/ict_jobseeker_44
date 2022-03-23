a6ea3455477c1efc126f131ce264f2ae09cc3835
SafrullahMohomed <emails3msm@gmail.com>
2022-03-23 07:22:03 +0530

    admin graph functions

--- controllers/Admin/Admin_home.php
+++ controllers/Admin/Admin_home.php
@@ -11,29 +11,44 @@ class Admin_home extends Controller
     function Admin_home()
     {
         error_reporting(0);
-        if($_SESSION['User_type'] == 'Admin44'){
+        if ($_SESSION['User_type'] == 'Admin44') {
             $this->view->renderAdmin('Admin_home');
-        }
-        else{
+        } else {
             $this->view->render('YouDontHavePermisson');
         }
 
 
-
     }
 
+//    get the job category counts
     function get_job_category()
     {
         $results = $this->model->get_job_category_m();
         echo json_encode($results);
     }
 
+//    get the contract category counts
+    function get_contract_category()
+    {
+        $results = $this->model->get_contract_category_m();
+        echo json_encode($results);
+    }
+
+//    get the user registered count in each month
+    function get_monthly_user_registered()
+    {
+        $results = $this->model->get_monthly_user_registered_m();
+        echo json_encode($results);
+    }
+
+//    get the total user
     function get_counts()
     {
         $results = $this->model->get_counts_m();
-            echo json_encode($results);
-
+        echo json_encode($results);
 
     }
+
+
 }
 
